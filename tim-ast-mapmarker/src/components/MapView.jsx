d24e72008c274f9e48b77b985861ee1c5ec9827f
Thahiramskji <thahirariska@gmail.com>
2026-04-24 16:37:54 +0700

    feat: render multiple markers (4 Banda Aceh locations)

--- tim-ast-mapmarker/src/components/MapView.jsx
+++ tim-ast-mapmarker/src/components/MapView.jsx
@@ -3,6 +3,31 @@ import "leaflet/dist/leaflet.css";
 import { Marker, Popup, Circle } from "react-leaflet";
 
 export default function MapView() {
+
+
+  const locations = [
+    {
+      name: "Masjid Raya Baiturrahman",
+      position: [5.5539, 95.3173],
+      address: "Jl. Moh. Jam No.1, Kp. Baru, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23127",
+    },
+    {
+      name: "Museum Tsunami Aceh",
+      position: [5.5477129, 95.3151901],
+      address: "Jl. Sultan Iskandar Muda No.3, Sukaramai, Kec. Baiturrahman, Kota Banda Aceh, Aceh 23116",
+    },
+    {
+      name: "Pantai Ulee Lheue",
+      position: [5.5643903, 95.2923091],
+      address: "Ulee Lheue, Kec. Meuraxa, Kota Banda Aceh, Aceh",
+    },
+    {
+      name: "Taman Sari Banda Aceh",
+      position: [5.5458870, 95.3161750],
+      address: "Jl. Merapi No.37, Sukaramai, Kec.Baiturrahman, Kota Banda Aceh, Aceh 23116",
+    },
+  ];
+
   return (
     <div style={{ height: "100vh" }}>
       <MapContainer
@@ -24,10 +49,25 @@ export default function MapView() {
             fillOpacity: 0.4,
           }}
         >
+
+
+
+{locations.map((loc, index) => (
+  <Marker key={index} position={loc.position}>
+    <Popup>
+      <div style={{ textAlign: "center" }}>
+        <h4>{loc.name}</h4>
+        <p>{loc.address}</p>
+      </div>
+    </Popup>
+  </Marker>
+))}
+
           <Popup>
             <div style={{ textAlign: "center" }}>
               <h3>Universitas Syiah Kuala</h3>
               <p>Kawasan Kampus USK</p>
+              <p>Jl. Teuku Nyak Arief No.441, Kopelma Darussalam, Kec. Syiah Kuala, Kota Banda Aceh, Aceh 23111</p>
             </div>
           </Popup>
         </Circle>
